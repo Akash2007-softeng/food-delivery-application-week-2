6e4e20970bc50013d50d3409b7b86e7d57ab439f
akash <akashbalaji740@gmail.com>
2026-07-03 10:51:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function login(){
+
+let username=document.getElementById("username").value;
+
+let password=document.getElementById("password").value;
+
+if(username=="" || password==""){
+
+alert("Please fill all fields");
+
+}
+else{
+
+alert("Login Successful");
+
+}
+
+}
